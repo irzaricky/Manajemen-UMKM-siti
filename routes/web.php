512154f5bbc58179b36850f268e2757a28bd1328
arsyirfza._ <irzasoul12@gmail.com>
2024-11-17 18:31:37 +0700

    perbaikan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ListProdukController;
+use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\BahanBakuController;
