1bcf245105f124828c82ce7bf8df3a4a682aee2c
arsyirfza._ <irzasoul12@gmail.com>
2024-11-23 23:18:57 +0700

    update tombol di beli bahan baku

--- resources/js/Pages/BahanBaku/Beli.vue
+++ resources/js/Pages/BahanBaku/Beli.vue
@@ -48,29 +48,6 @@ function submit() {
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6">
                         <!-- Tambah tombol back -->
-                        <div class="mb-4">
-                            <Link
-                                :href="route('bahan-baku.index')"
-                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-gray-700 bg-gray-100 rounded-md hover:bg-gray-200"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    class="w-5 h-5 mr-1"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M10 19l-7-7m0 0l7-7m-7 7h18"
-                                    />
-                                </svg>
-                                Kembali
-                            </Link>
-                        </div>
-
                         <h2 class="text-lg font-semibold mb-4">
                             Form Pembelian {{ bahanBaku.nama }}
                         </h2>
